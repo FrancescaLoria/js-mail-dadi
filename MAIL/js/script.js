1831edf8077322de51f068afb8a1c4b71d77342d
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-13 15:37:12 +0100

    mail js completato

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -14,4 +14,20 @@ console.log(registereds);
 
 const userMail = prompt("inserisci la tua mail").toLowerCase();
 
-console.log(userMail);
+//console.log(userMail);
+
+// 2. CONTROLLARE SE SIA NELLA LISTA DI CHI PUO' ACCEDERE
+let registrato = false;
+for (i = 0; i < registereds.length; i++) {
+  const currentEmail = registereds[i];
+  console.log(currentEmail);
+  if (userMail === currentEmail) {
+    registrato = true;
+  }
+}
+// STAMPARE MESSAGGIO SULL'ESITO DEL CONTROLLO
+if (registrato) {
+  console.log("Sei iscritto");
+} else {
+  console.log("Mi spiace, non sei iscritto!");
+}
